9ec17fcdea2b308040dfc9f453d72b052f8b4818
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-10-22 18:25:14 +0530

    add the code

--- src/router/adminRouter.js
+++ src/router/adminRouter.js
@@ -10,60 +10,67 @@ const getMulterStorage = require("../helper/fileUpload");
 // const uploadSingle = getMulterStorage("groupImage").single("groupImage"); 
 adminRouter.post("/login", adminController.login);
 adminRouter.get("/userList", varifyToken, adminController.userList);
+adminRouter.post("/changPassword", adminController.changPassword);
+adminRouter.post("/SendOtp", adminController.forgotPasswordSendOtp);
+adminRouter.post("/varifyOtp", adminController.varifyOtp);
 adminRouter.delete("/deleteUser", varifyToken, adminController.deleteUser);
-adminRouter.patch(
-  "/blockUnblockUser",
-  varifyToken,
-  adminController.blockUnblockUser
-);
 adminRouter.get(
   "/adminProfile",
   varifyToken,
   verifyRoles(roleList.ADMIN),
   adminController.adminProfile
 );
+// adminRouter.patch(
+//   "/blockUnblockUser",
+//   varifyToken,
+//   adminController.blockUnblockUser
+// );
 
-adminRouter.get(
-  "/totalActiveUser",
-  varifyToken,
-  verifyRoles(roleList.ADMIN),
-  adminController.totalActiveUser
-);
+// adminRouter.get(
+//   "/adminProfile",
+//   varifyToken,
+//   verifyRoles(roleList.ADMIN),
+//   adminController.adminProfile
+// );
 
-adminRouter.get(
-  "/totalDeactiveUser",
-  varifyToken,
-  verifyRoles(roleList.ADMIN),
-  adminController.totalDeactiveUser
-);
+// adminRouter.get(
+//   "/totalActiveUser",
+//   varifyToken,
+//   verifyRoles(roleList.ADMIN),
+//   adminController.totalActiveUser
+// );
 
-adminRouter.get(
-  "/totalPendingReq",
-  varifyToken,
-  verifyRoles(roleList.ADMIN),
-  adminController.totalPendingReq
-);
+// adminRouter.get(
+//   "/totalDeactiveUser",
+//   varifyToken,
+//   verifyRoles(roleList.ADMIN),
+//   adminController.totalDeactiveUser
+// );
 
-adminRouter.get(
-  "/blockUserList",
-  varifyToken,
-  verifyRoles(roleList.ADMIN),
-  adminController.blockUserList
-);
+// adminRouter.get(
+//   "/totalPendingReq",
+//   varifyToken,
+//   verifyRoles(roleList.ADMIN),
+//   adminController.totalPendingReq
+// );
+
+// adminRouter.get(
+//   "/blockUserList",
+//   varifyToken,
+//   verifyRoles(roleList.ADMIN),
+//   adminController.blockUserList
+// );
 
 
+// adminRouter.get(
+//   "/deshboardCount",
+//   varifyToken,
+//   verifyRoles(roleList.ADMIN),
+//   adminController.deshboardCount
+// );
 
-adminRouter.get(
-  "/deshboardCount",
-  varifyToken,
-  verifyRoles(roleList.ADMIN),
-  adminController.deshboardCount
-);
-adminRouter.post("/changPassword", adminController.changPassword);
-adminRouter.post("/SendOtp", adminController.forgotPasswordSendOtp);
-adminRouter.post("/varifyOtp", adminController.varifyOtp);
 
-adminRouter.post("/addUser", varifyToken, adminController.addUser);
+// adminRouter.post("/addUser", varifyToken, adminController.addUser);
 
 // adminRouter.post(
 //   "/addNotification",
